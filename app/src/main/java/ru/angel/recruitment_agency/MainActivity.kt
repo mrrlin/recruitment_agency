7d261f5ba0b37e11db3dfbb128c5d592c3a99f78
Angelina <bunny.iblis@gmail.com>
2023-01-02 14:39:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/angel/recruitment_agency/MainActivity.kt
@@ -0,0 +1,40 @@
+package ru.angel.recruitment_agency
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.angel.recruitment_agency.ui.theme.Recruitment_agencyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Recruitment_agencyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Recruitment_agencyTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/ru/angel/recruitment_agency/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ru.angel.recruitment_agency.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
