e102977921d7a52138e9ef07836fc398b3071ba7
Angelina <bunny.iblis@gmail.com>
2023-01-19 00:06:30 +0300

    Final fix

--- app/src/main/java/ru/angel/recruitment_agency/screens/Responces.kt
+++ app/src/main/java/ru/angel/recruitment_agency/screens/Responces.kt
@@ -1,16 +1,10 @@
 package ru.angel.recruitment_agency.screens
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,39 +13,19 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import ru.angel.recruitment_agency.MainViewModel
-import ru.angel.recruitment_agency.model.CV
-import ru.angel.recruitment_agency.navigation.NavRoute
-import ru.angel.recruitment_agency.utils.Constants
-import ru.angel.recruitment_agency.utils.DB_TYPE
-import ru.angel.recruitment_agency.utils.TYPE_FIREBASE
-import ru.angel.recruitment_agency.utils.TYPE_ROOM
 
 @Composable
-fun ResponsesScreen(navController: NavHostController, viewModel: MainViewModel) {
+fun ResponsesScreen() {
     Column {
         Text(text = "Hey, this is responses")
         ResponseItemAccepted()
         ResponseItemRejected()
         ResponseItemWaiting()
     }
-    //val cvs = viewModel.readAllCVs().observeAsState(listOf()).value
-//    Scaffold() {
-//        LazyColumn {
-//            items(cvs) { cv ->
-//                CVItem(cv = cv, navController = navController)
-//            }
-            
-//        }
-//    }
 }
 
 @Composable
 fun ResponseItemAccepted() {
-//    val cvId = when(DB_TYPE.value) {
-//        TYPE_FIREBASE -> cv.firebaseId
-//        TYPE_ROOM -> cv.id
-//        else -> Constants.Keys.EMPTY
-//    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -76,11 +50,6 @@ fun ResponseItemAccepted() {
 
 @Composable
 fun ResponseItemRejected() {
-//    val cvId = when(DB_TYPE.value) {
-//        TYPE_FIREBASE -> cv.firebaseId
-//        TYPE_ROOM -> cv.id
-//        else -> Constants.Keys.EMPTY
-//    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -105,11 +74,6 @@ fun ResponseItemRejected() {
 
 @Composable
 fun ResponseItemWaiting() {
-//    val cvId = when(DB_TYPE.value) {
-//        TYPE_FIREBASE -> cv.firebaseId
-//        TYPE_ROOM -> cv.id
-//        else -> Constants.Keys.EMPTY
-//    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
